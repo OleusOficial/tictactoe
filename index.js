0450a7f92182e78fc51a8d598c005b908f00f337
GO Now <androidkimbiso@gmail.com>
2018-12-27 17:01:17 -0600

    first Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http')
+var socketio = require('socket.io'); 
+
+var server = http.createServer();
+
+var io = socketio(server, {
+    path: '/',
+    //serveClient: false,
+    // below are engine.IO options
+    pingInterval: 5000,
+    pingTimeout: 3000,
+    cookie: false,
+});
+server.listen(3000, () => console.log('listening on *:3000'));
+
+
+io.on('connection', client => {
+    var sessionID = client.id; 
+    console.log('client connected: '+sessionID); 
+    
+    client.on('disconnect', () => {console.log('client has left: '+sessionID)});
+});
+
+
+  
